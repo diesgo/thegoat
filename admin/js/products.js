ce1ab7b68e6b784c5ca4b42676eb3d649a764d02
diesgo <diesgo@gmail.com>
2020-09-30 21:26:22 +0200

    Inicio del proyecto:
    CCMS
    
    Aplicacion para la gestion de asociaciones.
    
    Fase de maquetacion de las diferentes paginas de la parte administrativa.

---
+++ admin/js/products.js
@@ -0,0 +1,104 @@
+var id_0 = [{ image: "0", name: "Bubblegum", category: "Flowers", quantity: "2500", cost: "4", price: "12" }];
+var id_1 = [{ image: "1", name: "Amnesia", category: "Flowers", quantity: "1300", cost: "3", price: "9" }];
+var id_2 = [{ image: "2", name: "Gelatto", category: "Flowers", quantity: "500", cost: "7", price: "31" }];
+var id_3 = [{ image: "3", name: "Cream Caramel", category: "Extractions", quantity: "1100", cost: "3", price: "9" }];
+var id_4 = [{ image: "4", name: "Lemonrella", category: "Estractions", quantity: "800", cost: "7", price: "21" }];
+var id_5 = [{ image: "5", name: "Zushi", category: "Cali Flowers", quantity: "300", cost: "30", price: "90" }];
+var id_6 = [{ image: "6", name: "Coca cola", category: "Fresh", quantity: "150", cost: "0.45", price: "1" }];
+var id_7 = [{ image: "7", name: "Water", category: "Drinks", quantity: "200", cost: "0.15", price: "1" }];
+var id_8 = [{ image: "8", name: "Crunch", category: "Snack", quantity: "36", cost: "0.45", price: "1" }];
+var id_9 = [{ image: "9", name: "Ligther", category: "Parafernalia", quantity: "60", cost: "0.70", price: "1" }];
+var id_10 = [{ image: "10", name: "Sweets", category: "Snack", quantity: "64", cost: "0.20", price: "1" }];
+
+var productos = [id_0, id_1, id_2, id_3, id_4, id_5, id_6, id_7, id_8, id_9, id_10];
+
+var products = [
+    {
+        id: "0",
+        image: "0",
+        name: "Bubblegum",
+        category: "Flowers",
+        quantity: "2500",
+        cost: "4",
+        price: "12"
+    }, {
+        id: "1",
+        image: "1",
+        name: "Amnesia",
+        category: "Flowers",
+        quantity: "1300",
+        cost: "3",
+        price: "9"
+    }, {
+        id: "2",
+        image: "2",
+        name: "Gelatto",
+        category: "Flowers",
+        quantity: "500",
+        cost: "7",
+        price: "31"
+    }, {
+        id: "3",
+        image: "3",
+        name: "Cream Caramel",
+        category: "Extractions",
+        quantity: "1100",
+        cost: "3",
+        price: "9"
+    }, {
+        id: "4",
+        image: "4",
+        name: "Lemonrella",
+        category: "Estractions",
+        quantity: "800",
+        cost: "7",
+        price: "21"
+    }, {
+        id: "5",
+        image: "5",
+        name: "Zushi",
+        category: "Cali Flowers",
+        quantity: "300",
+        cost: "30",
+        price: "90"
+    }, {
+        id: "6",
+        image: "6",
+        name: "Coca cola",
+        category: "Fresh",
+        quantity: "150",
+        cost: "0.45",
+        price: "1"
+    }, {
+        id: "7",
+        image: "7",
+        name: "Water",
+        category: "Drinks",
+        quantity: "200",
+        cost: "0.15",
+        price: "1"
+    }, {
+        id: "8",
+        image: "8",
+        name: "Crunch",
+        category: "Snack",
+        quantity: "36",
+        cost: "0.45",
+        price: "1"
+    }, {
+        id: "9",
+        image: "9",
+        name: "Ligther",
+        category: "Parafernalia",
+        quantity: "60",
+        cost: "0.70",
+        price: "1"
+    }, {
+        id: "10",
+        image: "10",
+        name: "Sweets",
+        category: "Snack",
+        quantity: "64",
+        cost: "0.20",
+        price: "1"
+    }];
