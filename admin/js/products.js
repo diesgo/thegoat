3e3a7fb6308bcb34a87b5ac9cf5662acd5aae6d7
diesgo <diesgo@gmail.com>
2020-10-14 17:27:54 +0200

    Carrito de la compra en fase de adaptacion al criterio del proyecto

--- admin/js/products.js
+++ admin/js/products.js
@@ -8,6 +8,21 @@ var tipoDeVenta =[
         tipo_de_venta: "Ud/s."
     }
 ]
+
+var socios = [
+    {
+        id_socio: 0,
+        numero_socio: 1,
+        foto_socio: "img/s/0.png"
+        nombre_socio: "Aitor",
+        primer_apellido_socio: "Menta",
+        segundo_apellido_socio: "Fuerte",
+        fecha_de_nacimiento: "29/02/2002",
+        dni: "123654789X",
+        sexo: "hombre",
+        credito: 100
+    }
+]
 var categorias = [
     {
         id_categoria: 0,
@@ -50,99 +65,99 @@ var categorias = [
 var flores = [
 
     {
-        id_flor: 0,
-        imagen_flor: "0.jpg",
-        nombre_flor: "Bubblegum",
+        id_articulo: 0,
+        imagen_articulo: "img/p/flores/0.jpg",
+        nombre_articulo: "Bubblegum",
         variedad: "Indica",
         cantidad: 2500,
         precio_coste: 4,
         precio_venta: 12
     },
     {
-        id_flor: 1,
-        imagen_flor: "1.jpg",
-        nombre_flor: "Amnesia",
+        id_articulo: 1,
+        imagen_articulo: "img/p/flores/1.jpg",
+        nombre_articulo: "Amnesia",
         variedad: "Sativa",
         cantidad: 1300,
         precio_coste: 3,
         precio_venta: 9
     },
     {
-        id_flor: 2,
-        imagen_flor: "2.jpg",
-        nombre_flor: "Gelatto",
+        id_articulo: 2,
+        imagen_articulo: "img/p/flores/2.jpg",
+        nombre_articulo: "Gelatto",
         variedad: "Indica",
         cantidad: 500,
         precio_coste: 7,
         precio_venta: 21
     },
     {
-        id_flor: 3,
-        imagen_flor: "3.jpg",
-        nombre_flor: "Cream Caramel",
+        id_articulo: 3,
+        imagen_articulo: "img/p/flores/3.jpg",
+        nombre_articulo: "Cream Caramel",
         variedad: "Indica",
         cantidad: 1100,
         precio_coste: 3,
         precio_venta: 9
     },
     {
-        id_flor: 4,
-        imagen_flor: "4.jpg",
-        nombre_flor: "Lemonrella",
+        id_articulo: 4,
+        imagen_articulo: "img/p/flores/4.jpg",
+        nombre_articulo: "Lemonrella",
         variedad: "Sativa",
         cantidad: 800,
         precio_coste: 7,
         precio_venta: 21
     },
     {
-        id_flor: 5,
-        imagen_flor: "5.jpg",
-        nombre_flor: "Zushi",
+        id_articulo: 5,
+        imagen_articulo: "img/p/flores/5.jpg",
+        nombre_articulo: "Zushi",
         variedad: "Indica",
         cantidad: 300,
         precio_coste: 30,
         precio_venta: 90
     },
     {
-        id_flor: 6,
-        imagen_flor: "6.jpg",
-        nombre_flor: "Space jam",
+        id_articulo: 6,
+        imagen_articulo: "img/p/flores/6.jpg",
+        nombre_articulo: "Space jam",
         variedad: "Indica",
         cantidad: 150,
         precio_coste: 4,
         precio_venta: 12
     },
     {
-        id_flor: 7,
-        imagen_flor: "7.jpg",
-        nombre_flor: "Z Monkey",
+        id_articulo: 7,
+        imagen_articulo: "img/p/flores/7.jpg",
+        nombre_articulo: "Z Monkey",
         variedad: "Hybrid",
         cantidad: 2300,
         precio_coste: 2,
         precio_venta: 6
     },
     {
-        id_flor: 8,
-        imagen_flor: "8.jpg",
-        nombre_flor: "Green Poison",
+        id_articulo: 8,
+        imagen_articulo: "img/p/flores/8.jpg",
+        nombre_articulo: "Green Poison",
         variedad: "Indica",
         cantidad: 3600,
         precio_coste: 5,
         precio_venta: 15
     },
     {
-        id_flor: 9,
-        imagen_flor: "9.jpg",
-        nombre_flor: "Zkittle Pie #5",
+        id_articulo: 9,
+        imagen_articulo: "img/p/flores/9.jpg",
+        nombre_articulo: "Zkittle Pie #5",
         variedad: "Sativa",
         cantidad: 60,
         precio_coste: 3,
         precio_venta: 9
     },
     {
-        id_flor: 10,
-        imagen_flor: "10.jpg",
-        nombre_flor: "Critical bilbo",
+        id_articulo: 10,
+        imagen_articulo: "img/p/flores/10.jpg",
+        nombre_articulo: "Critical bilbo",
         variedad: "Indica",
         cantidad: 6400,
         precio_coste: 20,
@@ -150,39 +165,152 @@ var flores = [
     }
 ]
 
-var products=catalogo.length;
-console.log(catalogo);
-
-function productGrid(){
-    var tabla=document.getElementById('grid');
-    for (let i = 0; i < products; i++) {
-        var rejilla =   "<tr>"+
-                                    "<td style='width: 10%'>"+
-                                        "<label class='checkbox'>"+catalogo[i].id +
-                                            "<input type='checkbox'>"+
-                                            "<span class='check w3-text-theme w3-round'></span>"+
-                                        "</label>"+
-                                    "</td>"+
-                                    "<td style='width: 10%'>"+
-                                        "<img class='w3-image' src='../img/p/"+catalogo[i].image+".jpg' style='width: 40px'>"+
-                                    "</td>"+
-                                    "<td style='width: 20%' class='w3-left-align'>"+catalogo[i].name +"</td>"+
-                                    "<td style='width: 10%' class='w3-left-align'>"+catalogo[i].category+"</td>"+
-                                    "<td style='width: 10%'>"+catalogo[i].quantity+" gr</td>"+
-                                    "<td style='width: 10%'>"+catalogo[i].cost+" €/gr</td>"+
-                                    "<td style='width: 10%'>"+catalogo[i].price+" €/gr</td>"+
-                                    "<td style='width: 10%'>"+
-                                        "<label class='switch'>"+
-                                            "<input type='checkbox' checked>"+
-                                            "<span class='slider round'></span>"+
-                                        "</label></td>"+
-                                    "<td style='width:10%'>"+
-                                        "<div class='w3-bar'>"+
-                                            "<a class='w3-bar-item w3-button w3-hover-text-theme' href='' style='width: 50%'><i class='w3-xlarge fas fa-pencil-alt'></i></a>"+
-                                            "<a class='w3-bar-item w3-button w3-hover-text-theme' href='' style='width: 50%'><i class='w3-xlarge fas fa-trash-alt - alt'></i></a>"+
-                                        "</div>"+
-                                    "</td>"+
-                                "</tr>";
-        tabla.innerHTML+=rejilla;
+var snacks = [
+    {
+        id_articulo: 0,
+        imagen_articulo: "img/p/snack/0.jpg",
+        nombre_articulo: "Snickers",
+        variedad: "",
+        cantidad: 24,
+        precio_coste: 0.40,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 1,
+        imagen_articulo: "img/p/snack/1.jpg",
+        nombre_articulo: "Crunch",
+        variedad: "",
+        cantidad: 15,
+        precio_coste: 0.36,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 2,
+        imagen_articulo: "img/p/snack/2.jpg",
+        nombre_articulo: "Twix",
+        variedad: "",
+        cantidad: 10,
+        precio_coste: 0.48,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 3,
+        imagen_articulo: "img/p/snack/3.jpg",
+        nombre_articulo: "!Chips Ahoy",
+        variedad: "",
+        cantidad: 18,
+        precio_coste: 0.68,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 4,
+        imagen_articulo: "img/p/snack/4.jpg",
+        nombre_articulo: "KitKat",
+        variedad: "",
+        cantidad: 5,
+        precio_coste: 0.32,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 5,
+        imagen_articulo: "img/p/snack/5.jpg",
+        nombre_articulo: "Reese's",
+        variedad: "",
+        cantidad: 48,
+        precio_coste: 1.15,
+        precio_venta: 2.5
+    },
+    {
+        id_articulo: 6,
+        imagen_articulo: "img/p/snack/6.jpg",
+        nombre_articulo: "Hersheys white",
+        variedad: "",
+        cantidad: 48,
+        precio_coste: 0.78,
+        precio_venta: 2
+    },
+    {
+        id_articulo: 7,
+        imagen_articulo: "img/p/snack/7.jpg",
+        nombre_articulo: "Hersheys original",
+        variedad: "",
+        cantidad: 48,
+        precio_coste: 0.78,
+        precio_venta: 2
+    },
+    {
+        id_articulo: 8,
+        imagen_articulo: "img/p/snack/8.jpg",
+        nombre_articulo: "Nerds",
+        variedad: "",
+        cantidad: 36,
+        precio_coste: 0.90,
+        precio_venta: 2
+    },
+    {
+        id_articulo: 9,
+        imagen_articulo: "img/p/snack/9.jpg",
+        nombre_articulo: "Haribo",
+        variedad: "",
+        cantidad: 36,
+        precio_coste: 0.56,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 10,
+        imagen_articulo: "img/p/snack/10.jpg",
+        nombre_articulo: "Pringles",
+        variedad: "",
+        cantidad: 60,
+        precio_coste: 0.36,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 11,
+        imagen_articulo: "img/p/snack/11.jpg",
+        nombre_articulo: "Patatas fritas",
+        variedad: "",
+        cantidad: 24,
+        precio_coste: 0.24,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 12,
+        imagen_articulo: "img/p/snack/12.jpg",
+        nombre_articulo: "Bounty",
+        variedad: "",
+        cantidad: 23,
+        precio_coste: 0.56,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 13,
+        imagen_articulo: "img/p/snack/13.jpg",
+        nombre_articulo: "Skittles",
+        variedad: "",
+        cantidad: 48,
+        precio_coste: 0.38,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 14,
+        imagen_articulo: "img/p/snack/14.jpg",
+        nombre_articulo: "m&m",
+        variedad: "",
+        cantidad: 48,
+        precio_coste: 0.48,
+        precio_venta: 1
+    },
+    {
+        id_articulo: 15,
+        imagen_articulo: "img/p/snack/15.jpg",
+        nombre_articulo: "m&m",
+        variedad: "",
+        cantidad: 48,
+        precio_coste: 0.48,
+        precio_venta: 1
     }
-}
+]
+
+var products=catalogo.length;
+console.log(catalogo); 
--- js/carrito1.js
+++ js/carrito1.js
@@ -1,182 +1,250 @@
-function drinks() {
-    // Variables
-    let baseDeDatos = [
-        {
-            id: 1,
-            nombre: 'Coca-cola',
-            precio: 1,
-            imagen: 'img/p/f01.jpg'
-        },
-        {
-            id: 2,
-            nombre: 'Fanta naranja',
-            precio: 1,
-            imagen: 'img/p/f02.jpg'
-        },
-        {
-            id: 3,
-            nombre: 'Calabacin',
-            precio: 2.1,
-            imagen: 'https://source.unsplash.com/random/500x500/?zucchini&sig=3'
-        },
-        {
-            id: 4,
-            nombre: 'Fresas',
-            precio: 0.6,
-            imagen: 'https://source.unsplash.com/random/500x500/?burrs&sig=4'
-        }
-
-    ]
+window.onload = function () {  
     let $items = document.querySelector('#items');
     let carrito = [];
     let total = 0;
     let $carrito = document.querySelector('#carrito');
     let $total = document.querySelector('#total');
     let $botonVaciar = document.querySelector('#boton-vaciar');
-
+    
     // Funciones
+    
     function renderItems() {
-        for (let info of baseDeDatos) {
-            let miNodo =
-                "<div class='producto w3-col m3 w3-margin-bottom w3-padding'>" +
-                "<div class='w3-container w3-border w3-round' style='padding: 4px;'>" +
-                "<div class='w3-container'>" +
-                "<h4><b>" + info['nombre'] + "</b></h4>" +
-                "</div>" +
-                "<div class='w3-half' style='padding: 4px 0px'>" +
-                "<img class='w3-image' src='" + info['imagen'] + "' alt=''>" +
-                "</div>" +
-                "<div class='w3-half' style='padding: 4px'>" +
-                "<p><span class='w3-xxlarge'><b>" + info['precio'] + "</b></span> €/gr</p>" +
-                "<span class='w3-orange w3-text-dark-grey w3-round-xxlarge' style='padding: 2px 10px'><b>Indica</b></span><br>" +
-                "</div>" +
-                "<div>" +
-                "<p class='Comprar w3-button w3-block w3-theme w3-hover-theme'>Compar</p>" +
-                "</div>" +
-                "</div>" +
-                "</div>";
-            // Estructura
-            // let miNodo = document.createElement('div');
-            // miNodo.classList.add('w3-col m3 w3-margin-bottom w3-padding');
-            // Ficha
-            // let miNodoCard = document.createElement('div');
-            // miNodoCard.classList.add('w3-container w3-border w3-round');
-            // miNodoCard.style.padding="4px";
-            // let miNodoCardBody = document.createElement('div');
-            // miNodoCardBody.classList.add('w3-container');
-            // Titulo
-            // let miNodoTitle = document.createElement('h4');
-            // miNodoTitle.textContent = info['nombre'];
+        for (let info of  flores) {           
+            
+            // Ficha de producto
+            
+            let fichaArticulo = document.createElement('div');            
+            fichaArticulo.classList.add('producto', 'w3-col', 'm3', 'l3', 's12', 'w3-margin-bottom', 'w3-padding');
+            
+            // Contenedor
+            
+            let contPrin = document.createElement('div');
+            contPrin.classList.add('w3-container', 'w3-border', 'w3-round', 'marcoEstrecho');
+            
+            // Contenedor del título
+            
+            let contTitulo = document.createElement("div");
+            contTitulo.classList.add('w3-container');
+            
+            let nombreArticulo = document.createElement('h6');
+            nombreArticulo.classList.add('font-lemonada');
+            
+            let nombreArticuloDestacado = document.createElement('b');
+            nombreArticuloDestacado.textContent = info['nombre_articulo'];
+            
+            nombreArticulo.appendChild(nombreArticuloDestacado);
+            contTitulo.appendChild(nombreArticulo);
+            
             // Imagen
-            // let miNodoImagen = document.createElement('img');
-            // miNodoImagen.classList.add('w3-image');
-            // miNodoImagen.setAttribute('src', info['imagen']);
+            
+            let contImagen = document.createElement('div');
+            contImagen.classList.add('w3-half', 'w3-padding-4');
+            
+            let imagenArticulo = document.createElement('img');
+            imagenArticulo.classList.add('w3-image');
+            imagenArticulo.setAttribute('src', info['imagen_articulo']);
+            
+            contImagen.appendChild(imagenArticulo)
+
             // Precio
-            // let miNodoPrecio = document.createElement('p');
-            // let miNodoPrecioDestacado = document.createElement('span');
-            // miNodoPrecioDestacado.classList.add('w3-xxlarge');
-            // miNodoPrecioDestacado.textContent = info['precio'];
-            // miNodoPrecio.appendChild(miNodoPrecioDestacado);
-            // miNodoPrecio.textContent += ' €/gr';
-            // Boton 
-            // let miNodoBoton = document.createElement('w3-button');
-            // miNodoBoton.classList.add('w3-button', 'w3-theme');
-            // miNodoBoton.textContent = '+';
-            // miNodoBoton.setAttribute('marcador', info['id']);
-            // miNodoBoton.addEventListener('click', anyadirCarrito);
+            
+            let contPrecio = document.createElement('div');
+            contPrecio.classList.add('w3-half', 'marcoEstrecho');
+            
+            let precioArticulo = document.createElement('p');
+            precioArticulo.classList.add('w3-xlarge');
+            
+            let precioDestacado = document.createElement('b');
+            precioDestacado.textContent = info['precio_venta'];
+            
+            let unidadVenta = document.createElement('span');
+            unidadVenta.classList.add('w3-small');
+            unidadVenta.textContent = ' €/gr.';
+            
+            // Variedad
+            
+            let variedad = document.createElement('span');
+            variedad.classList.add('w3-orange', 'w3-text-dark-grey', 'w3-round-xxlarge', 'w3-padding-tiny');
+            
+            let variedadDestacada = document.createElement('b');
+            variedadDestacada.textContent = info['variedad'];
+            
+            variedad.appendChild(variedadDestacada);
+            precioArticulo.appendChild(precioDestacado);
+            precioArticulo.appendChild(unidadVenta);
+            contPrecio.appendChild(variedad);
+            contPrecio.appendChild(precioArticulo);
+            
+            // Boton
+            
+            let contBoton = document.createElement('div');
+            contBoton.classList.add('w3-container', 'w3-padding-0');
+            
+            let boton = document.createElement('button');
+            boton.classList.add('w3-button', 'w3-theme', 'w3-block');
+            boton.textContent = 'Comprar';
+            boton.setAttribute('marcador', info['id_articulo']);
+            boton.addEventListener('click', anyadirCarrito);
+            contBoton.appendChild(boton);
+            
             // Insertamos
-            // miNodoCardBody.appendChild(miNodoTitle);
-            // miNodoCard.appendChild(miNodoCardBody);
-            // miNodoCard.appendChild(miNodoPrecio);
-            // miNodoCard.appendChild(miNodoBoton);
-            // miNodo.appendChild(miNodoCard);
-            $items.appendChild(miNodo);
+            
+            contPrin.appendChild(contTitulo);
+            contPrin.appendChild(contImagen);
+            contPrin.appendChild(contPrecio);
+            contPrin.appendChild(variedad);
+            contPrin.appendChild(contBoton);
+            fichaArticulo.appendChild(contPrin);
+            $items.appendChild(fichaArticulo);
         }
     }
 
     function anyadirCarrito() {
+        
         // Anyadimos el Nodo a nuestro carrito
+        
         carrito.push(this.getAttribute('marcador'))
+        
         // Calculo el total
+        
         calcularTotal();
-        // Renderizamos el carrito 
+        
+        // Renderizamos el carrito
+        
         renderizarCarrito();
     }
-
+    
     function renderizarCarrito() {
+        
         // Vaciamos todo el html
+        
         $carrito.textContent = '';
+        
         // Quitamos los duplicados
+        
         let carritoSinDuplicados = [...new Set(carrito)];
+        
         // Generamos los Nodos a partir de carrito
+        
         carritoSinDuplicados.forEach(function (item, indice) {
+            
             // Obtenemos el item que necesitamos de la variable base de datos
-            let miItem = baseDeDatos.filter(function (itemBaseDatos) {
-                return itemBaseDatos['id'] == item;
+            
+            let miItem = flores.filter(function (itemBaseDatos) {
+                return itemBaseDatos['id_articulo'] == item;
             });
+            
             // Cuenta el número de veces que se repite el producto
+            
             let numeroUnidadesItem = carrito.reduce(function (total, itemId) {
                 return itemId === item ? total += 1 : total;
             }, 0);
+            
             // Creamos el nodo del item del carrito
-            let miNodo = document.createElement('li');
-            miNodo.classList.add('w3-right-align');
-            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0]['nombre']} - ${miItem[0]['precio']}€`;
+            
+            let fichaArticulo = document.createElement('tr');
+            fichaArticulo.classList.add('w3-white');
+            
+            let fichaArticulo2 = document.createElement('td');
+            fichaArticulo2.textContent = `${numeroUnidadesItem} gr`;
+            
+            let fichaArticulo3 = document.createElement('td');
+            fichaArticulo3.textContent = `${miItem[0]['nombre_articulo']}`;
+
+            let fichaArticulo4 = document.createElement('td');
+            fichaArticulo4.textContent = ` - ${miItem[0]['precio_venta']}€/gr`;
+            
+            fichaArticulo.appendChild(fichaArticulo2);
+            fichaArticulo.appendChild(fichaArticulo3);
+            fichaArticulo.appendChild(fichaArticulo4);
+            
             // Boton de borrar
-            let miBoton = document.createElement('w3-button');
-            miBoton.classList.add('w3-text-red');
-            miBoton.textContent = 'X';
+            
+            let miBoton = document.createElement('button');
+            miBoton.classList.add('w3-button', 'w3-white', 'w3-badge', 'w3-padding-tiny', 'font-haetten-schweiler');
+            
+            let miBotonTexto = document.createElement('b');
+            miBotonTexto.classList.add('w3-badge', 'w3-red');
+            miBotonTexto.textContent = 'X';
+            
+            miBoton.appendChild(miBotonTexto);
             miBoton.style.marginLeft = '1rem';
             miBoton.setAttribute('item', item);
             miBoton.addEventListener('click', borrarItemCarrito);
+            
             // Mezclamos nodos
-            miNodo.appendChild(miBoton);
-            $carrito.appendChild(miNodo);
+
+            fichaArticulo.appendChild(miBoton);
+            $carrito.appendChild(fichaArticulo);
         })
     }
-
+    
     function borrarItemCarrito() {
         console.log()
+        
         // Obtenemos el producto ID que hay en el boton pulsado
+        
         let id = this.getAttribute('item');
+        
         // Borramos todos los productos
+        
         carrito = carrito.filter(function (carritoId) {
             return carritoId !== id;
         });
+        
         // volvemos a renderizar
+        
         renderizarCarrito();
+        
         // Calculamos de nuevo el precio
+        
         calcularTotal();
     }
-
+    
     function calcularTotal() {
+        
         // Limpiamos precio anterior
+        
         total = 0;
+        
         // Recorremos el array del carrito
+        
         for (let item of carrito) {
+            
             // De cada elemento obtenemos su precio
-            let miItem = baseDeDatos.filter(function (itemBaseDatos) {
-                return itemBaseDatos['id'] == item;
+            
+            let miItem = flores.filter(function (itemBaseDatos) {
+                return itemBaseDatos['id_flor'] == item;
             });
-            total = total + miItem[0]['precio'];
+            total = total + miItem[0]['precio_venta'];
         }
+        
         // Formateamos el total para que solo tenga dos decimales
+        
         let totalDosDecimales = total.toFixed(2);
+        
         // Renderizamos el precio en el HTML
+        
         $total.textContent = totalDosDecimales;
     }
-
+    
     function vaciarCarrito() {
+        
         // Limpiamos los productos guardados
+        
         carrito = [];
+        
         // Renderizamos los cambios
+        
         renderizarCarrito();
         calcularTotal();
     }
-
+    
     // Eventos
+    
     $botonVaciar.addEventListener('click', vaciarCarrito);
-
+    
     // Inicio
+    
     renderItems();
-} 
+}
